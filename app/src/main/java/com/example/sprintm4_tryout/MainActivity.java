86c43ff830f6f7f801232b48d1f69a0e03c4453f
UrzuaAlarcon <bea.urzua.a@gmail.com>
2023-03-31 21:50:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sprintm4_tryout/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sprintm4_tryout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentTransaction transaction;
+    Fragment fragment_home, fragment_packages;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment_home = new home_fragment();
+        fragment_packages = new viajes_fragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, fragment_home).commit();
+
+    }
+
+    public void travelPackages(View view) {
+
+        transaction= getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragmentContainer, fragment_packages);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+}
