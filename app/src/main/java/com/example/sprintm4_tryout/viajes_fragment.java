e39866c0c964b3182073da444d00134ed4e5e13f
UrzuaAlarcon <bea.urzua.a@gmail.com>
2023-04-15 13:15:53 -0400

    actualizacion 5.1

--- app/src/main/java/com/example/sprintm4_tryout/viajes_fragment.java
+++ app/src/main/java/com/example/sprintm4_tryout/viajes_fragment.java
@@ -26,6 +26,7 @@ public class viajes_fragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
